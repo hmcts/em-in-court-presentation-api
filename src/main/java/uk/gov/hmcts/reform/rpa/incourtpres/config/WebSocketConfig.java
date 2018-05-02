8743ca3ca61b11137322e1a19c5d6ca423e02ea5
Louis Houghton <louis@louishoughton.com>
2018-05-02 14:57:10 +0100

    Allow connections from anywhere

--- src/main/java/uk/gov/hmcts/reform/rpa/incourtpres/config/WebSocketConfig.java
+++ src/main/java/uk/gov/hmcts/reform/rpa/incourtpres/config/WebSocketConfig.java
@@ -18,7 +18,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("icp/ws").withSockJS();
+        registry
+                .addEndpoint("icp/ws")
+                .setAllowedOrigins("*")
+                .withSockJS();
     }
 
 }
