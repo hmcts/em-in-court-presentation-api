7e2a6c826708aa439d9993d8f4090316972b83c3
Louis Houghton <louis@louishoughton.com>
2018-05-08 13:38:18 +0100

    Hearing controller (#2)
    * Add controller to create session
    
    * Save documents in session

---
+++ src/main/java/uk/gov/hmcts/reform/rpa/incourtpres/controllers/HearingSessionController.java
@@ -0,0 +1,37 @@
+package uk.gov.hmcts.reform.rpa.incourtpres.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import uk.gov.hmcts.reform.rpa.incourtpres.domain.HearingSession;
+import uk.gov.hmcts.reform.rpa.incourtpres.services.HearingSessionService;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.UUID;
+
+@RestController()
+@RequestMapping("icp/sessions")
+public class HearingSessionController {
+
+    private HearingSessionService hearingSessionService;
+
+    @Autowired
+    public HearingSessionController(HearingSessionService hearingSessionService) {
+        this.hearingSessionService = hearingSessionService;
+    }
+
+    @PostMapping(value = "", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<HearingSession> newSession(
+            @RequestBody final HearingSession hearingSession) throws URISyntaxException {
+        String location = "/icp/sessions/" + this.hearingSessionService.newSession(hearingSession).getId();
+        return ResponseEntity.created(new URI(location)).body(hearingSession);
+    }
+
+    @GetMapping(value = "{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<HearingSession> getSession(@PathVariable UUID id) {
+        return ResponseEntity.ok(this.hearingSessionService.getSession(id));
+    }
+}
